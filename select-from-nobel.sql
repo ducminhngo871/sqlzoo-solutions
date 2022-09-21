86b5ef84bb1e56f3bc37b0ed834edf5b54bdc58d
Duc Ngo <ducminhngo871@gmail.com>
2022-09-21 16:42:53 -0500

    Completed!

--- select-from-nobel.sql
+++ select-from-nobel.sql
@@ -87,7 +87,7 @@ SELECT *
 FROM nobel
 WHERE winner = 'Eugene O''Neill'
 
--- 13. Knights in order
+--- 13. Knights in order
 
 List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
 
